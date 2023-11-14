e39a0b3fb544b7c79f136d11a6e41555eeebcd15
kariuki11 <irungukenneth433@gmail.com>
2023-11-14 02:40:20 -0800

    The disturbing file

--- print_%.c
+++ print_%.c
@@ -1,11 +1,36 @@
-#include "main.h"
-/**
- * print_39 - prints the char %.
- * Return: 1.
- */
-int print_39(void)
-{
-	_putchar(39);
-	return (1);
-}
+#include <stdio.h>
+#include <stdarg.h>
 
+int _printf(const char *format, ...) {
+    va_list args;
+    int count = 0;
+
+    va_start(args, format);
+
+    while (*format != '\0') {
+        if (*format == '%' && *(format + 1) != '\0') {
+            format++;
+            switch (*format) {
+                case 'c':
+                    count += putchar(va_arg(args, int));
+                    break;
+                case 's':
+                    count += printf( va_arg(args, char *));
+                    break;
+                case '%':
+                    count += putchar('%');
+                    break;
+                default:
+                    count += putchar('%');  
+                    count += putchar(*format);
+            }
+        } else {
+            count += putchar(*format);
+        }
+        format++;
+    }
+
+    va_end(args);
+
+    return count;
+}
